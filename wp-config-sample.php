427a4ffa0aaa08b172603e797f2ed8ff4c87ed4c
premiumski <118439644+premiumski@users.noreply.github.com>
2022-11-17 13:52:39 +0200

    update database

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'beddsa3h2qxpwdvl2ude' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'ui3znxbtscs0zbtt' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'lheB86wDWhhY00FF99ql' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'beddsa3h2qxpwdvl2ude-mysql.services.clever-cloud.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
