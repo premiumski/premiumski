5fe202b2d4c0b100c7b9f31e62e4295094250fad
premiumski <118439644+premiumski@users.noreply.github.com>
2022-11-30 16:29:07 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'bw87zc1cmycevdxf2les' );
+define( 'DB_NAME', 'buh8zh7gq6tl2vmitywt' );
 
 /** Database username */
-define( 'DB_USER', 'uliqvkkcdf6jijuy' );
+define( 'DB_USER', 'u7zppd1cda4ho0ka' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'sjo0t7RKNKORez0xjP8n' );
+define( 'DB_PASSWORD', 'V6xsSuqbQzFs80aewSy' );
 
 /** Database hostname */
-define( 'DB_HOST', 'bw87zc1cmycevdxf2les-mysql.services.clever-cloud.com' );
+define( 'DB_HOST', 'buh8zh7gq6tl2vmitywt-mysql.services.clever-cloud.com' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
