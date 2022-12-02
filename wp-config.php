b6f38b8fa20f016d94d43cbd7b466c20be92e5ba
premiumski <118439644+premiumski@users.noreply.github.com>
2022-12-02 11:47:20 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -48,6 +48,7 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
+define( 'DB_HOST', getenv("MYSQL_ADDON_HOST").':'.getenv("MYSQL_ADDON_PORT") );
 define( 'AUTH_KEY',         '}.EYJRm4[fvCw{<DB|JD@BK:Xj@}m}(R{l PUd2sZ#jB`|k-=/4ajP=!P3v1YnTJ' );
 define( 'SECURE_AUTH_KEY',  ':fF4L.Rtkn7BtFA<:yj2VQUYc?RSl{`Mb<n/S~<CzdF 4TrpurT9S9[T{slH$)>1' );
 define( 'LOGGED_IN_KEY',    '?D nIYdB-XX:1d&$iShF.J2|tB&NUx9^s`vA:x/r#6c(1rAQ}&/AA=>/m[+f<M g' );
